9aad8e09051452eb2a816f2b9423e9a724cdc7c3
Saxon Dean <saxond@pm.me>
2026-01-18 12:47:09 +1100

    Add MoreNorms

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
+
+use lina::{Point2, Scalar};
+use num::{Num, Signed};
+
+pub mod bucket;
 pub mod debug;
 pub mod grid;
 pub mod parse;
-pub mod bucket;
 pub mod union_find;
 
 pub trait ResultExt<T> {
@@ -15,6 +19,29 @@ impl<T> ResultExt<T> for Result<T, T> {
     }
 }
 
-#[cfg(test)]
-mod tests {
+pub trait MoreNormDistance {
+    type Field;
+    fn distance_manhattan(&self, other: Self) -> Self::Field;
+    fn distance_inf(&self, other: Self) -> Self::Field;
 }
+
+impl<C: Signed + Num + Scalar + Ord> MoreNormDistance for Point2<C> {
+    type Field = C;
+    fn distance_manhattan(&self, other: Self) -> C {
+        (*self - other)
+            .map(|x| x.abs())
+            .iter()
+            .fold(C::zero(), |a, b| a + b)
+    }
+
+    fn distance_inf(&self, other: Self) -> C {
+        (*self - other)
+            .map(|x| x.abs())
+            .iter()
+            .max()
+            .unwrap_or(C::zero())
+    }
+}
+
+#[cfg(test)]
+mod tests {}
